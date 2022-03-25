af700a9b01db6e5ebe82f7c71a7e286eb0b5cefb
LeoDog896 <LeoDog896@hotmail.com>
2022-03-25 17:14:24 +0000

    CI cleanup

--- demo/src/routes/index.svelte
+++ demo/src/routes/index.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
   import { renderText, renderTwoTone } from "../../../src/index"
+  interface RenderSystem {
+    name: string,
+    render: (value: string) => string,
+    lineSpacing: string,
+    tracking: string
+  }
 
-  const renderSystems = [{
+  const renderSystems: RenderSystem[] = [{
     name: "Unicode",
     render: renderTwoTone,
     lineSpacing: "1.1rem",
@@ -18,26 +24,33 @@
   let value = ""
 
 </script>
-<div class="flex flex-row h-screen w-screen">
-  <div class="h-full flex-grow">
-    <input placeholder="https://example.com" class="flex-grow w-full text-center" bind:value={value}>
-    <h1 class="font-mono text-center my-10" style="
-    line-height: {selectedRenderSystem.lineSpacing};
-    letter-spacing: {selectedRenderSystem.tracking}
-    ">
-      {@html selectedRenderSystem
-        .render(value || "https://example.com")
-        .replaceAll("\n", "<br/>")
-        .replaceAll(" ", "&nbsp;")
-      }
-    </h1>
+<div class="flex flex-col w-screen h-screen">
+  <div class="flex flex-row flex-grow w-full">
+    <div class="h-full flex-grow m-8">
+      <textarea tabindex=0
+        placeholder="Type URL here (EX: https://example.com)"
+        class="flex-grow w-full text-center" bind:value={value}
+      />
+      <h1 class="font-mono text-center my-10" style="
+      line-height: {selectedRenderSystem.lineSpacing};
+      letter-spacing: {selectedRenderSystem.tracking}
+      ">
+        {@html selectedRenderSystem
+          .render(value)
+          .replaceAll("\n", "<br/>")
+          .replaceAll(" ", "&nbsp;")
+        }
+      </h1>
+    </div>
+    <div class="flex-shrink flex flex-col w-32 border-l-4 border-gray-300">
+      {#each renderSystems as renderSystem, i}
+        <div tabindex={i + 1} class="
+          w-full {selectedRenderSystem == renderSystem ? "bg-gray-200" : "bg-gray-100"} hover:bg-gray-300
+          hover:cursor-pointer transition-colors p-4
+        " on:click={() => {selectedRenderSystem = renderSystem}}>{renderSystem.name}</div>
+      {/each}
+    </div>
   </div>
-  <div class="flex-shrink flex flex-col w-32 border-l-4 border-gray-300">
-    {#each renderSystems as renderSystem, i}
-      <div tabindex={i} class="
-        w-full {selectedRenderSystem == renderSystem ? "bg-gray-200" : "bg-gray-100"} hover:bg-gray-300
-        hover:cursor-pointer transition-colors p-4
-      " on:click={() => {selectedRenderSystem = renderSystem}}>{renderSystem.name}</div>
-    {/each}
+  <div class="flex-row">
   </div>
 </div>
