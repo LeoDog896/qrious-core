4015638c39c3c057485b85c1291447f56a6ffaa6
LeoDog896 <LeoDog896@hotmail.com>
2022-03-31 16:22:50 +0000

    onMount

--- demo/src/routes/__layout.svelte
+++ demo/src/routes/__layout.svelte
@@ -1,10 +1,13 @@
 <script>
   import { renderText, generateFrame } from '../../../src/qr';
+  import { onMount } from "svelte"
 
-  globalThis.renderText = renderText
-  globalThis.generateFrame = generateFrame
+  onMount(() => {
+    window["renderText"] = renderText
+    globalThis.generateFrame = generateFrame
 
-  console.log("Welcome to scannable! The renderText and generateFrame functions are now available as a global variable. Feel free to play around with it!")
+    console.log("Welcome to scannable! The renderText and generateFrame functions are now available as a global variable. Feel free to play around with it!")
+  })
 
 	import '../app.css';
 </script>
