55ba436387eb619de722a385b2a1936c6f12d790
LeoDog896 <LeoDog896@hotmail.com>
2022-06-06 14:24:32 +0000

    Move types to seperate places

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { renderCanvas, renderContext } from './qr/renderer/renderCanvas';
 export { renderTwoTone } from './qr/renderer/renderTwoTone';
 export type { ImageLikeRenderOptions } from './qr/renderer/options/image';
 export { defaultImageLikeRenderOptions } from './qr/renderer/options/image';
-export type { FrameOptions, UserFacingFrameOptions } from './qr/Frame';
-export { generateFrame, defaultFrameOptions, type MaskType } from './qr/Frame';
+export type { FrameOptions, UserFacingFrameOptions, MaskType } from './qr/Frame';
+export { generateFrame, defaultFrameOptions } from './qr/Frame';
